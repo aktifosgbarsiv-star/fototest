17f2cc59f6fa5c0b669a68243761ef99c1a3cdce
aktifosgbarsiv-star <aktifosgbarsiv@gmail.com>
2026-06-18 13:57:16 +0300

    feat: egitimler - SiteFloating eklendi

--- app/egitimler/page.tsx
+++ app/egitimler/page.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from 'react'
 import SiteNav from '@/components/site/SiteNav'
 import SiteFooter from '@/components/site/SiteFooter'
+import SiteFloating from '@/components/site/SiteFloating'
 import { createClient } from '@/lib/supabase'
 export const dynamic = 'force-dynamic'
 
@@ -39,6 +40,7 @@ export default function Egitimler() {
           ))}
         </div>
       </div>
+      <SiteFloating />
       <SiteFooter />
     </div>
   )
